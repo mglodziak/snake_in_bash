4f649d7abe1513ae275ce33ced7dfca6eddffa94
mglodziak <mikolaj0608@gmail.com>
2021-04-10 00:02:20 +0200

    prawie final

--- game.sh
+++ game.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
 function get_config {
-cat snake.conf | grep $1 | cut -d' ' -f2
+  cat snake.conf | grep $1 | cut -d' ' -f2
 }
 
 
 function set_variables_at_start {
-var_width_game=`get_config WIDTH`
-var_height_game=`get_config HEIGHT`
-var_width_game_half=$[$var_width_game/2]
-var_height_game_half=$[$var_height_game/2]
-var_snake_length=`get_config SNAKE_START_LENGTH`
-var_score=`get_config SCORE`
-var_number_of_food=`get_config NUMBER_OF_FOOD`
+  var_width_game=`get_config WIDTH`
+  var_height_game=`get_config HEIGHT`
+  var_width_game_half=$[$var_width_game/2]
+  var_height_game_half=$[$var_height_game/2]
+  var_snake_length=`get_config SNAKE_START_LENGTH`
+  var_score=`get_config SCORE`
+  var_number_of_food=`get_config NUMBER_OF_FOOD`
+  var_score_multiplier=`get_config SCORE`
+  var_number_of_monsters=`get_config NUMBER_OF_MONSTERS`
+  var_blank=`get_config BLANK`
+  var_blank=$[var_blank-10]
 
-#initializing positions variables
-var_position_x=0
-var_position_y=0
+  #initializing positions variables
+  var_player_position_x=0
+  var_player_position_y=0
 
-#initializing food positions
-for (( i=1; $i<=$var_number_of_food; i++ )); do
-  tab_eat_position_x[$i]=0 #eat positions
-  tab_eat_position_y[$i]=0
+  #initializing food positions
+  for (( i=1; $i<=$var_number_of_food; i++ )); do
+    tab_food_position_x[$i]=0 #food positions
+    tab_food_position_y[$i]=0
+  done
+
+  for (( i=1; $i<=$var_number_of_monsters; i++ )); do
+tab_monster_position_x[$i]=0 #monster positions
+tab_monster_position_y[$i]=0
+tab_monster_blank[$i]=0
+
+
 done
 }
 
@@ -44,7 +56,7 @@ function make_board {
 
 function show_board_mono {
   echo -e "\n\n\n\n\n"
-  printf "\t\t     Score: $var_score\n\n"
+  printf "Score: $var_score\n\n"
   cat board
 }
 
@@ -52,7 +64,13 @@ function show_board_mono {
 function show_board_colored {
   echo -e "\n\n\n\n\n"
   printf "\t\t     Score: $var_score\n\n"
-  echo -e `cat board | sed 's/ /*/g'| sed 's/X/\\\033[41;31mX\\\033[0m/g' | sed 's/o/\\\033[0;32mo\\\033[0m/g'` | tr ' ' '\n' | tr '*' ' '
+  echo -e `cat board |\
+  sed 's/^/        /' |\
+  sed 's/ /*/g'|\
+  sed 's/X/\\\033[46;36mX\\\033[0m/g' |\
+  sed 's/o/\\\033[1;32mo\\\033[0m/g' |\
+  sed 's/#/\\\033[1;31m#\\\033[0m/g'` |\
+  tr ' ' '\n' | tr '*' ' '
 }
 
 
@@ -60,8 +78,8 @@ function set_player_start_point {
   var_start_x=$1
   var_start_y=$2
   var_object=$3
-  var_position_x=$var_start_x
-  var_position_y=$var_start_y
+  var_player_position_x=$var_start_x
+  var_player_position_y=$var_start_y
 
   gawk -v start_y="$var_start_y"\
   -v start_x="$var_start_x"\
@@ -73,8 +91,8 @@ function set_player_start_point {
 
 function end_game {
   echo -e "\n"
-  printf "\t\t Game over!\n\n "
-  printf "\t"
+  printf "\t\t   Game over!\n\n "
+  printf "\t\t"
   read -p "Enter your name: " name
   date=`date +"%H:%M  %d.%m.%y"`
   echo -e "$name \t $var_score \t $date" >> best_scores.txt
@@ -84,7 +102,7 @@ function end_game {
 }
 
 
-function delete_old_P {
+function delete_old_position {
   gawk -v x="$1"\
   -v y="$2"\
   -v height="$var_height_game"\
@@ -92,7 +110,7 @@ function delete_old_P {
 }
 
 
-function set_new_P {
+function set_new_position {
   gawk -v x="$1"\
   -v y="$2"\
   -v object=$3\
@@ -100,77 +118,98 @@ function set_new_P {
   -i inplace -F "" 'BEGINFILE{NF=height; OFS=""} {if(NR==y) {$x=object} {print $0}}' board
 }
 
-
-function move_player {
-  if [[ $var_position_x -eq 1 ||\
-  $var_position_y -eq 1 ||\
-  $var_position_x -eq $var_width_game  ||\
-  $var_position_y -eq $var_height_game ]];
-  then
-    end_game
-
-  else
-    read -t 0.1 -n 1 key
-
-    if [[ ! -z $key ]]; then
-      key2=$key
-    fi
-    delete_old_P $var_position_x $var_position_y
-    case $key2 in
-
-      w|W)
-      var_position_y=$[$var_position_y-1] ;;
-
-      a|A)
-      var_position_x=$[$var_position_x-1] ;;
-
-      s|S)
-      var_position_y=$[$var_position_y+1] ;;
-      d|D)
-      var_position_x=$[$var_position_x+1] ;;
-
-      *) move_player  ;;
-
-    esac
-    is_kill2
-    set_new_P $var_position_x $var_position_y '@'
-  fi
-}
-
-
-function set_eat_start_points {
-  for (( i=1; $i<=$var_number_of_food; i++ )); do
-
-    random_eat_xy
-    var_start_x=${tab_eat_position_x[$i]}
-    var_start_y=${tab_eat_position_y[$i]}
-    var_object=$1
-
-    gawk -v start_y="$var_start_y"\
-    -v start_x="$var_start_x"\
-    -v height="$var_height_game"\
-    -v object="$var_object"\
-    -i inplace -F "" 'BEGINFILE{NF=height; OFS=""} {if(NR==start_y) {$start_x=object} {print $0}}' board
-
+function set_monsters_blank {
+  for (( i=1; $i<=$var_number_of_monsters; i++ )); do
+    tab_monster_blank[$i]=$(( $RANDOM % $var_blank + 11))
   done
 }
 
 
-function stay_eats {
+function move_monsters {
+  for (( i=1; $i<=$var_number_of_monsters; i++ )); do
+    tab_monster_blank[$i]=$[${tab_monster_blank[$i]}-1]
+
+		if [[ ${tab_monster_blank[$i]} -eq 0 ]]; then
+      delete_old_position ${tab_monster_position_x[$i]} ${tab_monster_position_y[$i]}
+      random_xy_one_monster
+      tab_monster_position_x[$i]=$var_monster_tmp_x
+      tab_monster_position_y[$i]=$var_monster_tmp_y
+      set_new_position $var_monster_tmp_x $var_monster_tmp_y '#'
+      tab_monster_blank[$i]=$(( $RANDOM % $var_blank + 11))
+  #usuń pozycję i stwórz nową
+		fi
+  done
+}
+
+function move_player {
+  if [[ $var_player_position_x -eq 1 ||\
+  $var_player_position_y -eq 1 ||\
+  $var_player_position_x -eq $var_width_game  ||\
+  $var_player_position_y -eq $var_height_game ]];
+  then
+    end_game
+  else
+    read -t 0.1 -n 1 key
+    if [[ ! -z $key ]]; then
+      key2=$key
+    fi
+    delete_old_position $var_player_position_x $var_player_position_y
+    case $key2 in
+      w|W)
+      var_player_position_y=$[$var_player_position_y-1] ;;
+      a|A)
+      var_player_position_x=$[$var_player_position_x-1] ;;
+      s|S)
+      var_player_position_y=$[$var_player_position_y+1] ;;
+      d|D)
+      var_player_position_x=$[$var_player_position_x+1] ;;
+      *) move_player  ;;
+    esac
+    is_killed_yourself
+    set_new_position $var_player_position_x $var_player_position_y '@'
+  fi
+}
+
+
+function set_food_start_points {
   for (( i=1; $i<=$var_number_of_food; i++ )); do
-    set_new_P ${tab_eat_position_x[$i]} ${tab_eat_position_y[$i]} 'o'
+    random_xy_one_food
+    tab_food_position_x[$i]=$var_food_tmp_position_x
+    tab_food_position_y[$i]=$var_food_tmp_position_y
+  done
+}
+
+function set_monster_start_points {
+  for (( i=1; $i<=$var_number_of_monsters; i++ )); do
+
+    random_xy_one_monster
+    tab_monster_position_x[$i]=$var_monster_tmp_x
+    tab_monster_position_y[$i]=$var_monster_tmp_y
+  done
+}
+
+function put_food {
+  for (( i=1; $i<=$var_number_of_food; i++ )); do
+    set_new_position ${tab_food_position_x[$i]} ${tab_food_position_y[$i]} 'o'
+  done
+}
+
+
+function put_monsters {
+  for (( i=1; $i<=$var_number_of_monsters; i++ )); do
+    set_new_position ${tab_monster_position_x[$i]} ${tab_monster_position_y[$i]} '#'
   done
 }
 
 
 function is_eaten {
   for (( i=1; $i<=$var_number_of_food; i++ )); do
-    if [[ $var_position_x -eq ${tab_eat_position_x[$i]} && $var_position_y -eq ${tab_eat_position_y[$i]} ]]; then
-      var_score=$[$var_score+1]
+    if [[ $var_player_position_x -eq ${tab_food_position_x[$i]} && $var_player_position_y -eq ${tab_food_position_y[$i]} ]]; then
+      var_score=$[$var_score+$var_score_multiplier]
       var_snake_length=$[$var_snake_length+1]
       random_xy_generator
-      tab_eat_position_x[$i]=$var_random_x
-      tab_eat_position_y[$i]=$var_random_y
+      tab_food_position_x[$i]=$var_random_x
+      tab_food_position_y[$i]=$var_random_y
     fi
   done
 }
@@ -182,39 +221,65 @@ function random_xy_generator {
 }
 
 
-function random_eat_xy {
-  for (( i=1; $i<=$var_number_of_food; i++ )); do
+function random_xy_one_monster {
     random_xy_generator
     while [[ 1 ]]; do
       if [[ $var_random_x -gt 23 && $var_random_x -lt 29 && $var_random_y -gt 8 && $var_random_y -lt 14 ]]; then
         random_xy_generator
       else
-        tab_eat_position_x[$i]=$var_random_x
-        tab_eat_position_y[$i]=$var_random_y
+        var_monster_tmp_x=$var_random_x
+        var_monster_tmp_y=$var_random_y
         break
       fi
     done
-  done
+}
+
+
+
+function random_xy_one_food {
+    random_xy_generator
+    while [[ 1 ]]; do
+      if [[ $var_random_x -gt 23 && $var_random_x -lt 29 && $var_random_y -gt 8 && $var_random_y -lt 14 ]]; then
+        random_xy_generator
+      else
+        var_food_tmp_position_x=$var_random_x
+        var_food_tmp_position_y=$var_random_y
+        break
+      fi
+    done
 }
 
 
 function build_snake {
   tmp_x=`tac positions | sed -n "2,2"p | tr ' ' '\n' | head -1`
   tmp_y=`tac positions | sed -n "2,2"p | tr ' ' '\n' | tail -1`
-  set_new_P $tmp_x $tmp_y 'O'
+  set_new_position $tmp_x $tmp_y 'O'
   tmp_x=`tac positions | sed -n "$var_snake_length,$var_snake_length"p | tr ' ' '\n' | head -1`
   tmp_y=`tac positions | sed -n "$var_snake_length,$var_snake_length"p | tr ' ' '\n' | tail -1`
-  delete_old_P $tmp_x $tmp_y
+  delete_old_position $tmp_x $tmp_y
 }
 
 
-function is_kill2 {
-  cat board | gawk -v x="$var_position_x"\
-  -v y="$var_position_y"\
+function is_dead {
+	for (( i=1; $i<=$var_number_of_monsters; i++ )); do
+
+if [[ $var_player_position_x -eq ${tab_monster_position_x[$i]} && $var_player_position_y -eq ${tab_monster_position_y[$i]} ]]; then
+        gawk -i inplace -F "" 'OFS="" { for(i=1;i<=NF;i++) {if($i=="o") $i=" "}} {print $0}' board
+	clear
+	show_board_colored
+	end_game
+fi
+done
+}
+
+
+function is_killed_yourself {
+  cat board | gawk -v x="$var_player_position_x"\
+  -v y="$var_player_position_y"\
   -v object='O'\
   -v height="$var_height_game"\
-  -F "" 'BEGINFILE{NF=height; OFS=""} {if(NR==y && $x=="O") {print "1"}}' > xxx
-  tmp=`cat xxx`
+  -F "" 'BEGINFILE{NF=height; OFS=""} {if(NR==y && $x=="O") {print "1"}}' > tmp_file
+  tmp=`cat tmp_file`
   if [[ $tmp -eq 1 ]]; then
     printf "\n You ate yourself...\n"
     end_game
@@ -227,15 +292,20 @@ function game {
   set_variables_at_start
   make_board
   set_player_start_point $var_width_game_half $var_height_game_half '@'
-  echo "$var_position_x $var_position_y" >> positions
-  set_eat_start_points 'o'
+  set_food_start_points
+  set_monster_start_points
+  set_monsters_blank
+  echo "$var_player_position_x $var_player_position_y" >> positions
 
   while [[ 1 ]]; do
-    stay_eats
+    put_food
+    put_monsters
     move_player
-    echo "$var_position_x $var_position_y" >> positions
+    echo "$var_player_position_x $var_player_position_y" >> positions
     is_eaten
     build_snake
+    is_dead
+    move_monsters
     clear
     #show_board_mono
     show_board_colored
--- start.sh
+++ start.sh
@@ -1,102 +1,121 @@
 #!/bin/bash
 
-var_no_monsters=`cat count_of_monsters`
-var_no_eats=`cat count_of_eats`
+function get_config {
+cat snake.conf | grep $1 | cut -d' ' -f2
+}
+
 
 function show_menu {
-	
 	clear
 	echo -e "\n\n\t\t MENU\n"
 	echo "1) play"
 	echo "2) how to play"
-
+	echo "3) change difficult [`get_config DIFFICULT`]"
 	echo "4) best scores"
 	echo "5) credits"
 	echo "q) quit"
-	
-
 }
 
 
 function best_scores {
- clear
-                echo -e "\n\n\t\t BEST SCORES\n"
-                cat best_scores.txt | column -t | sort -n -r -k2 | head -20
-                echo -e "\npress any key to quit best scores" 
-                read -n 1 x
-                if [[ ! -z $x ]]; then
-                show_menu
-                menu
-                fi 
+	clear
+	echo -e "\n\n\t\t BEST SCORES\n"
+	cat best_scores.txt | column -t | sort -n -r -k2 | head -20
+	echo -e "\npress any key to quit best scores"
+	read -n 1 x
+	if [[ ! -z $x ]]; then
+		show_menu
+		menu
+	fi
 }
 
-function instruction {
-clear
-echo -e "\n\n\t\t INSTRUCTION\n"
-echo "Snake's head is '@'"
-echo "Snake is 'O'"
-echo "Eat is 'o'"
-echo ""
-echo "press 'w' to move up"
-echo "press 's' to move down"
-echo "press 'd' to move right"
-echo "press 'a' to move left"
-echo ""
-echo "press any other key to pause the game"
-echo "game ends if you crash with 'X' (end of chart) or eat yourself"
-echo -e "\npress any key to quit instructions"
-read -n 1 x
-  if [[ ! -z $x ]]; then
-                show_menu
-                menu
-                fi
 
+function instruction {
+	clear
+	echo -e "\n\n\t\t INSTRUCTION\n"
+	echo "Snake's head is '@'"
+	echo "Snake is 'O'"
+	echo "Eat is 'o'"
+	echo
+	echo "press 'w' to move up"
+	echo "press 's' to move down"
+	echo "press 'd' to move right"
+	echo "press 'a' to move left"
+	echo
+	echo "press any other key to pause the game"
+	echo "game ends if you crash with 'X' (end of chart) or eat yourself"
+	echo -e "\npress any key to quit instructions"
+	read -n 1 x
+	if [[ ! -z $x ]]; then
+		show_menu
+		menu
+	fi
 }
 
 
 function credits {
-clear
-echo -e "\n\n\t\t CREDITS\n"
-echo "Author: Mikolaj Glodziak"
-echo "Special thanks to Daria Gryla for testing, patience and assistance"
-echo ""
-echo "Copyrights by Mikolaj Glodziak, AGH - University of science and technology, Cracow, Poland, 2019"
-echo -e "\npress any key to quit credits"
-read -n 1 x
-  if [[ ! -z $x ]]; then
-                show_menu
-                menu
-                fi
-
-
+	clear
+	echo -e "\n\n\t\t CREDITS\n"
+	echo "Author: Mikolaj Glodziak"
+	echo "This game is a part of Udemy course: "
+	read -n 1 x
+	if [[ ! -z $x ]]; then
+		show_menu
+		menu
+	fi
 }
+
+
+function change_difficult {
+current_difficult=`get_config DIFFICULT`
+if [[ $current_difficult = "easy" ]]; then
+	sed -i 's/DIFFICULT easy/DIFFICULT medium/' snake.conf
+	sed -i 's/NUMBER_OF_MONSTERS 0/NUMBER_OF_MONSTERS 4/' snake.conf
+	sed -i 's/NUMBER_OF_FOOD 12/NUMBER_OF_FOOD 9/' snake.conf
+	sed -i 's/SCORE 1/SCORE 2/' snake.conf
+
+elif [[ $current_difficult = "medium" ]]; then
+	sed -i 's/DIFFICULT medium/DIFFICULT hard/' snake.conf
+	sed -i 's/NUMBER_OF_MONSTERS 4/NUMBER_OF_MONSTERS 8/' snake.conf
+	sed -i 's/NUMBER_OF_FOOD 9/NUMBER_OF_FOOD 6/' snake.conf
+	sed -i 's/SCORE 2/SCORE 3/' snake.conf
+
+elif [[ $current_difficult = "hard" ]]; then
+	sed -i 's/DIFFICULT hard/DIFFICULT easy/' snake.conf
+	sed -i 's/NUMBER_OF_MONSTERS 8/NUMBER_OF_MONSTERS 0/' snake.conf
+	sed -i 's/NUMBER_OF_FOOD 6/NUMBER_OF_FOOD 12/' snake.conf
+	sed -i 's/SCORE 3/SCORE 1/' snake.conf
+fi
+
+show_menu
+menu
+}
+
 
 function menu {
-echo  ""
-read -n 1 -p "Your choice: " choice
+	echo  ""
+	read -n 1 -p "Your choice: " choice
 
-case $choice in
-	1) 	echo -e "\npress 'w' or 'a' or 's' or 'd' to start game"
-		./game.sh $var_no_monsters $var_no_eats ;;
+	case $choice in
+		1) 	echo -e "\npress 'w' or 'a' or 's' or 'd' to start game"
+		./game.sh ;;
 
-	2) instruction ;;
+		2) instruction ;;
 
-	
+		3) change_difficult ;;
 
-	4) best_scores ;;
+		4) best_scores ;;
 
-	5) credits ;;
+		5) credits ;;
 
-	q|Q) 	echo ""
+		q|Q) 	echo ""
 		exit ;;
 
-	*) echo "Wrong choice. Try one more time." 
+		*) echo "Wrong choice. Try one more time."
 		show_menu
 		menu ;;
-esac
+	esac
 }
-
-printf "\n\n Welcome to snake bash-game!\n\n"
 
 show_menu
 menu
